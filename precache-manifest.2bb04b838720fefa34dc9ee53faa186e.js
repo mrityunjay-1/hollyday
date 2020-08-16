abe7f24c6386f9d119db6b8e48423b363ad240a4
Mrityunjay Kumar <kumarmrityunjay7034@gmail.com>
2020-08-16 13:39:31 +0530

    Updates

---
+++ precache-manifest.2bb04b838720fefa34dc9ee53faa186e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389b6f80fc2270bd826c6e85b5c6367d",
+    "url": "/hollyday/index.html"
+  },
+  {
+    "revision": "5a086aa433b90dc693cf",
+    "url": "/hollyday/static/css/main.a25869f4.chunk.css"
+  },
+  {
+    "revision": "6a19df0ff8d50fef5a33",
+    "url": "/hollyday/static/js/2.a04159d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hollyday/static/js/2.a04159d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a086aa433b90dc693cf",
+    "url": "/hollyday/static/js/main.837e4161.chunk.js"
+  },
+  {
+    "revision": "f680ff476c77144d026c",
+    "url": "/hollyday/static/js/runtime-main.8f842bab.js"
+  }
+]);
